43d7dfb54f6595deedae5c4d5848787398be07c5
Adam Madueno <amadueno17@yahoo.com>
2020-08-27 17:28:12 -0700

    added handle bars npm

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,7 @@ USE burger_db;
 CREATE TABLE burgers (
   id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(30) UNIQUE NOT NULL
+  devoured BOOL NOT NULL
+  
   
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,7 @@
   
-USE grocery_db;
+USE burger_db;
 
-INSERT INTO groceries (name, quantity, cost, purchased)
-VALUES ('Milk', 1, 3.00, false),
-  ('Carrots', 5, 1.50, false),
-  ('Cereal', 1, 3, true),
-  ('Oranges', 5, 3.25, true),
-  ('Bread', 2, 4.50, false),
-  ('Candy', 1, 1.25, false),
-  ('Oreos', 1, 2.50, true),
-  ('Cookies', 1, 2.50, true),
-  ('Eggs', 1, 4, false),
-  ('Yogurt', 1, 3.5, false);
+INSERT INTO burgers (name,devoured )
+VALUES ('californiaburger', false),
+  ('doubleburger', false),
+  ('hawaiinburger',false);
