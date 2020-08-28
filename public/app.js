cc0476c2caf29da10749c2bce0a7f5eb26b61f63
Adam Madueno <amadueno17@yahoo.com>
2020-08-27 22:24:16 -0700

    fixed issued hopefully

--- public/app.js
+++ public/app.js
@@ -1,58 +1,59 @@
 document.getElementById('addBurger').addEventListener('click', event => {
     event.preventDefault()
-  
+
     axios.post('/api/burgers', {
-      name: document.getElementById('burger').value,
-      
-     devoured: false
+        name: document.getElementById('product').value,
+
+        devoured: false
     })
-      .then(({ data }) => {
-        let burgerElem = document.createElement('li')
-        burgerElem.className = 'list-group-item'
-        burgerElem.id = data.id
-        burgerElem.innerHTML = `
-         <div class="d-flex w-100 justify-content-between">
-           <h5 class="mb-1">${document.getElementById('burger').value}</h5>
-           <button 
-            data-name="${document.getElementById('burger').value}"
+        .then(({ data }) => {
+            let burgerElem = document.createElement('li')
+            burgerElem.className = 'list-group-item'
+            burgerElem.id = data.id
+            burgerElem.innerHTML = `
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${document.getElementById('product').value}</h5>
+        <button 
+        data-name="${document.getElementById('product').value}"
+         class="devour btn btn-success">Devour it!</button>
+        </div>
+      
+     
+        `
+            document.getElementById('notDevoured').append(burgerElem)
+
+            document.getElementById('product').value = ''
             
-            class="devour btn btn-success">Devour</button>
-         </div>
-         
-        `
-        document.getElementById('notDevoured').append(burgerElem)
-  
-        document.getElementById('burger').value = ''
-        
-      })
-      .catch(err => console.error(err))
-  })
-  
-  document.addEventListener('click', event => {
+        })
+        .catch(err => console.error(err))
+})
+
+document.addEventListener('click', event => {
     if (event.target.classList.contains('devour')) {
-      axios.put(`/api/burgers/${event.target.parentNode.parentNode.id}`, {
-        devoured: true
-      })
-        .then(() => {
-          let burgerElem = document.createElement('li')
-          burgerElem.className = 'list-group-item'
-          burgerElem.id = event.target.parentNode.parentNode.id
-          burgerElem.innerHTML = `
-         <div class="d-flex w-100 justify-content-between">
-           <h5 class="mb-1">${event.target.dataset.name}</h5>
-           <button class="btn btn-danger remove">X</button>
-         </div>
-         
+        axios.put(`/api/burgers/${event.target.parentNode.parentNode.id}`, {
+            devoured: true
+        })
+            .then(() => {
+                let burgerElem = document.createElement('li')
+                burgerElem.className = 'list-group-item'
+                burgerElem.id = event.target.parentNode.parentNode.id
+                burgerElem.innerHTML = `
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${event.target.dataset.name}</h5>
+       <button class="btn btn-danger remove">Delete</button>
+        </div>
+      
+      
         `
-          document.getElementById('Devoured').append(burgerElem)
-          event.target.parentNode.parentNode.remove()
-        })
-        .catch(err => console.error(err))
+                document.getElementById('devoured').append(burgerElem)
+                event.target.parentNode.parentNode.remove()
+            })
+            .catch(err => console.error(err))
     } else if (event.target.classList.contains('remove')) {
-      axios.delete(`/api/burgers/${event.target.parentNode.parentNode.id}`)
-        .then(() => {
-          event.target.parentNode.parentNode.remove()
-        })
-        .catch(err => console.error(err))
+        axios.delete(`/api/burgers/${event.target.parentNode.parentNode.id}`)
+            .then(() => {
+                event.target.parentNode.parentNode.remove()
+            })
+            .catch(err => console.error(err))
     }
-  })
+})
